d8672474c41692faca2986de1fce94ac51483df1
Ameya Mahesh Shinde <93002372+gr1mw1k@users.noreply.github.com>
2023-03-06 00:14:01 +0530

    Delete instagram directory

--- instagram/instagram/src/main/java/com/geekster/instagram/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.geekster.instagram.dao;
-
-import com.geekster.instagram.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
